cef3155a177bb46d8e094fc3a3875316b6846d4a
phibar <philippbartels@outlook.com>
2021-11-02 05:57:50 +0100

    clean

--- src/GitHubLoader.ts
+++ src/GitHubLoader.ts
@@ -18,8 +18,7 @@ export class GitHubLoader {
     this.componentsDir = path.join("Components", "Github");
     this.mkDirRecursive(this.componentsDir);
     this.git = simpleGit();
-    this.git.status().then(result=>console.log("status",result))
-  //  console.log("status",this.git.status())
+    this.git.status().then((result) => console.log("status", result));
   }
 
   async loadUcpComponents() {
@@ -27,21 +26,22 @@ export class GitHubLoader {
       q: "topic:ucp-component",
     });
 
-    result.data.items.forEach((repo) => {
+    for (let repo of result.data.items) {
       const repoPath = path.join(this.componentsDir, repo.full_name);
       const companyPath = path.parse(repoPath).dir;
       this.mkDirRecursive(companyPath);
 
       if (fs.existsSync(repoPath)) {
-        console.log("update")
-        this.git.cwd(repoPath).pull()
+        console.log("update");
+        await this.git.cwd(repoPath).pull();
       } else {
-        this.git.submoduleAdd(repo.ssh_url,repoPath).then(f=>console.log(f))
-        
+        await this.git
+          .submoduleAdd(repo.ssh_url, repoPath)
+          .then((f) => console.log(f));
       }
-    });
+    }
   }
-  
+
   private mkDirRecursive(dir: string) {
     !fs.existsSync(dir) && fs.mkdirSync(dir, { recursive: true });
   }
